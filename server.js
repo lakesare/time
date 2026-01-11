43f9577ecae442b8da4c20ae59961c451fc47ff3
Evgenia Karunus <lakesare@gmail.com>
2026-01-11 04:10:51 +0100

    some more

--- server.js
+++ server.js
@@ -15,17 +15,36 @@ const server = https.createServer(options, (req, res) => {
     // Set the Permissions-Policy header
     res.setHeader('Permissions-Policy', 'screen-wake-lock=(self)');
 
-    // Serve the pomodoro.html file
-    const filePath = path.join(__dirname, 'pomodoro.html');
+    // Determine which file to serve
+    let filePath;
+    let contentType;
+
+    if (req.url === '/' || req.url === '/pomodoro.html') {
+        filePath = path.join(__dirname, 'pomodoro.html');
+        contentType = 'text/html';
+    } else if (req.url === '/shimmer.mp3') {
+        filePath = path.join(__dirname, 'shimmer.mp3');
+        contentType = 'audio/mpeg';
+    } else if (req.url === '/manifest.json') {
+        filePath = path.join(__dirname, 'manifest.json');
+        contentType = 'application/json';
+    } else if (req.url === '/sw.js') {
+        filePath = path.join(__dirname, 'sw.js');
+        contentType = 'application/javascript';
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
+        return;
+    }
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error loading pomodoro.html');
+            res.end('Error loading file');
             return;
         }
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': contentType });
         res.end(data);
     });
 });
